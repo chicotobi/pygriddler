14104a0ae42dcca80c330f241e205f5707a00df4
chicotobi <tobias310788@googlemail.com>
2024-03-01 21:31:02 +0100

    simplify

--- script.py
+++ script.py
@@ -11,7 +11,10 @@ def generate(n, blocks, start=False):
   l = []
   nblocks = len(blocks)
   max_shift = n - sum(blocks) - nblocks + 1
-  i0 = 1
+  if start:
+    i0 = 0
+  else:
+    i0 = 1
   for i in range(i0,max_shift+1):
     l += [[0]*i + [1]*blocks[0] + j for j in generate(n-i-blocks[0],blocks[1:])]
   return l
@@ -19,14 +22,10 @@ def generate(n, blocks, start=False):
 pos = {'h':{},'v':{}}
 for i in range(x):
   print("x",i/x)
-  tmp = generate(y+1, tuple(inp_v[i]), True)
-  tmp = [j[1:] for j in tmp]
-  pos['v'][i] = tmp
+  pos['v'][i] = generate(y, tuple(inp_v[i]), True)
 for i in range(y):
   print("y",i/y)
-  tmp = generate(x+1, tuple(inp_h[i]), True)
-  tmp = [j[1:] for j in tmp]
-  pos['h'][i] = tmp
+  pos['h'][i] = generate(x, tuple(inp_h[i]), True)
 
 solution = [[-1 for i in range(x)] for j in range(y)]
 
