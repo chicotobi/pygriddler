beca5b518251c894b36dc40fb77b59aa0a96e29d
hofmant3 <tobias.hofmann.th1@roche.com>
2024-03-04 13:10:17 +0100

    stuff

--- myfuncs.py
+++ myfuncs.py
@@ -1,9 +1,10 @@
 import urllib.request
 from functools import cache
 import os.path
-import PIL
 import numpy as np
- 
+import matplotlib.pyplot as plt 
+import matplotlib.colors
+
 def download_and_write_file(id0):
   s1 = 'https://www.griddlers.net/nonogram/-/g/t1709243262226/i01?p_p_lifecycle=2&p_p_resource_id=griddlerPuzzle&p_p_cacheability=cacheLevelPage&_gpuzzles_WAR_puzzles_id='
   s2 = '&_gpuzzles_WAR_puzzles_lite=false&_gpuzzles_WAR_puzzles_name=touchScreen'
@@ -66,13 +67,46 @@ def generate_count(n, block_lengths, block_colors, previous_color):
     l += tmp
   return l
 
-def hex2rgb(hx):
-  return tuple(int(hx[i:i+2], 16) for i in (0, 2, 4))
+@cache
+def generate_with_info(n, block_lengths, block_colors, previous_color, info):
+  if len(block_lengths) == 0:
+    if all(info[:n,0]) == 1:
+      return [[0]*n]
+  l = []
+  n_same_colored_neighbours = sum(np.diff(block_colors)==0)
+  max_zeroes_left_side = n - sum(block_lengths) - n_same_colored_neighbours
+  if block_colors[0] == previous_color:
+    i0 = 1
+  else:
+    i0 = 0
+  for i in range(i0,max_zeroes_left_side+1):
+    tmp = generate(n - i - block_lengths[0], block_lengths[1:], block_colors[1:], block_colors[0])
+    l += [[0]*i + [block_colors[0]]*block_lengths[0] + j for j in tmp]
+  return l
 
-def plot(x, y, s, colors):
-  im = PIL.Image.new(mode="RGB", size=(x, y))
+def hex2rgb(hx):
+  return tuple(int(hx[i:i+2], 16)/256 for i in (0, 2, 4))
+
+def plot(color_possible, colors, ori):
   
+  plt.clf()
+  if ori == 1:    
+    color_possible = np.transpose(color_possible, axes=(1,0,2))
+      
+  colors = ['808080'] + colors
+  cmap = [hex2rgb(i) for i in colors]
+  cmap = matplotlib.colors.ListedColormap(cmap)
+  
+  x, y, ncolors = color_possible.shape
+  data = -1 * np.ones((x, y))
   for i in range(x):
     for j in range(y):
-      im[i][j] = hex2rgb(colors[s[i][j]])
-  im.show()
+      if sum(color_possible[i,j,:]) == 1:
+        #print(np.where(color_possible[i,j,:]))
+        data[i,j] = np.where(color_possible[i,j,:])[0] + 1
+      else:
+        data[i,j] = -1
+  plt.imshow(data, interpolation='nearest', cmap = cmap,  vmin=-1, vmax=len(colors))
+  plt.gca().get_xaxis().set_visible(False)
+  plt.gca().get_yaxis().set_visible(False)
+  plt.show()
--- script.py
+++ script.py
@@ -12,40 +12,67 @@ inp, colors = get_input(id0)
 
 limit_generate = 1_000_000
 
-pos = {}
+status = {}
 for ori in range(2):
-  pos[ori] = {}
+  status[ori] = {}
   n_lines = len(inp[ori])
   len_line = len(inp[1-ori])
   for line in range(n_lines):
+    status[ori][line] = {}
     block_colors  = tuple([j[0] for j in inp[ori][line]])
     block_lengths = tuple([j[1] for j in inp[ori][line]])
     n_pos = generate_count(len_line, block_lengths, block_colors, -1)
     if n_pos < limit_generate:
-      pos[ori][line] = generate(len_line, block_lengths, block_colors, -1)
+      status[ori][line]["possible_lines"] = generate(len_line, block_lengths, block_colors, -1)
+      status[ori][line]["generated"     ] = True
+      status[ori][line]["count"         ] = n_pos
       print("Orientation",ori,"Line",line,": Generated",n_pos,"possibilities.")
     else:
-      pos[ori][line] = -1
+      status[ori][line]["possible_lines"] = None
+      status[ori][line]["generated"     ] = False
+      status[ori][line]["count"         ] = n_pos
       print("Orientation",ori,"Line",line,": Counted",n_pos,"possibilities.")
 
 x = len(inp[0])
 y = len(inp[1])
-color_possible = np.ones((x,y,len(colors)-1))
+n_colors = len(colors)
+color_possible = np.ones((y,x,n_colors))
   
+
 while np.any(np.sum(color_possible, axis=2)>1):
-  for ori, pos0 in pos.items():
-    for idx, line in pos0.items():
-      # Update from solution
-      for idx2, val in enumerate(solution[idx,]):
-        if val != -1:
-          line = [i for i in line if i[idx2] == val]
+  #old = color_possible.copy()
+  for ori, pos0 in status.items():
+    for idx, status0 in pos0.items():      
+      #assert(color_possible.shape[0] == len(possible_lines0[0]))      
+      #assert(color_possible.shape[1] == len(pos0))
+      if not status0["generated"]:
+        continue
       
-      print("Look at", ori, idx)
+      # Remove lines in pos, depending on solution
+      possible_lines0 = status0["possible_lines"]
+      for color in range(n_colors):
+        for idx2, val in enumerate(color_possible[:,idx,color]):
+          if val == 0:
+            possible_lines0 = [i for i in possible_lines0 if i[idx2] != color]
+      status0["count"] = len(possible_lines0)
       
-      # Now see if these solutions have given values  
-      uniques = [(idx2,i.pop()) for idx2,i in enumerate(map(set, zip(*line))) if len(i)==1]
-      for idx2, val in uniques:
-        solution[idx][idx2] = val
-      #plot(x, y, solution, colors)
-    solution = solution.transpose()
+      print("Look at O", ori, "L", idx,": Possible solutions:",status0["count"])
+      
+      # Update color_possible
+      allowed_colors = list(map(set, zip(*possible_lines0)))
+      for idx2, allowed_colors0 in enumerate(allowed_colors):
+        for color in range(n_colors):
+          if color not in allowed_colors0:
+            color_possible[idx2,idx,color] = 0
+      plot(color_possible, colors, ori)
+    color_possible = np.transpose(color_possible, axes=(1,0,2))
+    
+  # # No updates?
+  # if np.all(old == color_possible):
+  #   print("No update to color_possible: Generate new solutions")
+    
+  #   for ori, pos0 in possible_lines.items():
+  #     for idx, possible_lines0 in pos0.items():
+  #       if isinstance(possible_lines[0][1],int):
+          
   
