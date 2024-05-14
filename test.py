04581733c8ca9134b0da36f48ea72c68f4e77f6d
hofmant3 <tobias.hofmann.th1@roche.com>
2024-05-14 09:08:09 +0200

    tests outcommented currently

--- test.py
+++ test.py
@@ -1,37 +1,40 @@
-from myfuncs import generate, generate_with_info
-import numpy as np
+# from generators import generate_count, generate
 
-
-def pr0(x):
-  s = '\n'.join(''.join(str(j) for j in i) for i in x)
-  print(s)
+# def pr0(x):
+#   s = '\n'.join(''.join(str(j) for j in i) for i in x)
+#   print(s)
   
-# Test case
-n = 10
-block_lengths = (2, 3)
-block_colors = (1, 1)
-previous_color = -1
+# # Test case
+# n = 30
+# block_lengths = (2, 2, 3, 3)
+# block_colors = (1, 1, 1, 1)
+# previous_color = -1
+# ans = generate(n, block_lengths, block_colors, previous_color)
+# pr0(ans)
 
-ans = generate(n, block_lengths, block_colors, previous_color)
-pr0(ans)
+# n = 40
+# block_lengths = (2, 2, 3, 3, 4, 4)
+# block_colors = (1, 1, 2, 2, 2, 2)
+# previous_color = -1
+# ans = generate(n, block_lengths, block_colors, previous_color)
+# pr0(ans)
 
-info = np.ones((n,2))
-info[4,1] = 0
-ans = generate_with_info(n, block_lengths, block_colors, previous_color, info)
-pr0(ans)
+# n = 50
+# block_lengths = (1, 1, 1, 1, 1, 1, 1, 1, 1, 1)
+# block_colors = (1, 1, 1, 1, 1, 1, 1, 1, 1, 1)
+# previous_color = -1
+# ans = generate_count(n, block_lengths, block_colors, previous_color)
+# pr0(ans)
 
 # info = np.ones((n,2))
 # info[4,1] = 0
 # ans = generate_with_info(n, block_lengths, block_colors, previous_color, info)
 # pr0(ans)
 
-
-# Test case
 # n = 7
 # block_lengths = (3,)
 # block_colors = (1,)
 # previous_color = -1
-
 # ans = generate(n, block_lengths, block_colors, previous_color)
 # pr0(ans)
 
