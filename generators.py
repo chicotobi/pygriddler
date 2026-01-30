a67eae7aa39c44710d4fe96443f7ac5b75b516c2
Tobias Hofmann (rm2mw061244) <tobias.hofmann.th1@roche.com>
2026-01-30 12:59:02 +0100

    better names in generators

--- generators.py
+++ generators.py
@@ -9,7 +9,7 @@ WHITE = 0
 def generate(n, block_lengths, block_colors, previous_color=-1):
     if len(block_lengths) == 0:
         return np.zeros((1, n), dtype=np.uint8)
-    l = []
+    possible_lines = []
     n_same_colored_neighbours = sum(np.diff(block_colors) == 0)
     max_zeroes_left_side = n - sum(block_lengths) - n_same_colored_neighbours
     if block_colors[0] == previous_color:
@@ -27,15 +27,15 @@ def generate(n, block_lengths, block_colors, previous_color=-1):
         a1 = np.zeros((nrow, i), dtype=np.uint8)
         a2 = np.ones((nrow, block_lengths[0]), dtype=np.uint8) * block_colors[0]
         a = np.concatenate((a1, a2, a3), axis=1)
-        l.append(a)
-    return np.concatenate(l)
+        possible_lines.append(a)
+    return np.concatenate(possible_lines)
 
 
 @cache
 def generate_count(n, block_lengths, block_colors, previous_color=-1):
     if len(block_lengths) == 0:
         return 1
-    l = 0
+    count = 0
     n_same_colored_neighbours = sum(np.diff(block_colors) == 0)
     max_zeroes_left_side = n - sum(block_lengths) - n_same_colored_neighbours
     if block_colors[0] == previous_color:
@@ -49,8 +49,8 @@ def generate_count(n, block_lengths, block_colors, previous_color=-1):
             block_colors=block_colors[1:],
             previous_color=block_colors[0],
         )
-        l += tmp
-    return l
+        count += tmp
+    return count
 
 
 @cache
@@ -65,19 +65,19 @@ def generate_from_slice(
             return np.zeros((1, n), dtype=np.uint8)
         else:
             return np.zeros((0, n), dtype=np.uint8)
-    pos = []
+    possible_lines = []
     n_same_colored_neighbours = sum(np.diff(block_colors) == 0)
     max_zeroes_left_side_from_input = n - sum(block_lengths) - n_same_colored_neighbours
 
     # Find the first index where no white is allowed
-    tmp = np.nonzero(slice[:, WHITE] == False)[0]
+    tmp = np.nonzero(not slice[:, WHITE])[0]
     if len(tmp) > 0:
         max_zeroes_left_side_from_slice = tmp[0]
     else:
         max_zeroes_left_side_from_slice = n
 
-    l = block_lengths[0]
-    c = block_colors[0]
+    length0 = block_lengths[0]
+    color0 = block_colors[0]
 
     max_zeroes_left_side = min(
         max_zeroes_left_side_from_input, max_zeroes_left_side_from_slice
@@ -88,23 +88,23 @@ def generate_from_slice(
         i0 = 0
     for i in range(i0, max_zeroes_left_side + 1):
         # We checked that the white blocks are allowed, now check, if the colored block is allowed:
-        if all(slice[i : (i + l), c]):
+        if all(slice[i : (i + length0), color0]):
             a3 = generate_from_slice(
-                n=n - i - l,
+                n=n - i - length0,
                 n_colors=n_colors,
                 block_lengths=block_lengths[1:],
                 block_colors=block_colors[1:],
-                slice=totuple(slice[i + l :,]),
-                previous_color=c,
+                slice=totuple(slice[i + length0 :,]),
+                previous_color=color0,
             )
             nrow, _ = a3.shape
             if a3.shape[0] > 0:
                 a1 = np.zeros((nrow, i), dtype=np.uint8)
-                a2 = np.ones((nrow, block_lengths[0]), dtype=np.uint8) * block_colors[0]
+                a2 = np.ones((nrow, length0), dtype=np.uint8) * color0
                 a = np.concatenate((a1, a2, a3), axis=1)
-                pos.append(a)
-    if len(pos) > 0:
-        return np.concatenate(pos)
+                possible_lines.append(a)
+    if len(possible_lines) > 0:
+        return np.concatenate(possible_lines)
     else:
         return np.zeros((0, n), dtype=np.uint8)
 
@@ -126,14 +126,14 @@ def generate_count_from_slice(
     max_zeroes_left_side_from_input = n - sum(block_lengths) - n_same_colored_neighbours
 
     # Find the first index where no white is allowed
-    tmp = np.nonzero(slice[:, WHITE] == False)[0]
+    tmp = np.nonzero(not slice[:, WHITE])[0]
     if len(tmp) > 0:
         max_zeroes_left_side_from_slice = tmp[0]
     else:
         max_zeroes_left_side_from_slice = n
 
-    l = block_lengths[0]
-    c = block_colors[0]
+    length0 = block_lengths[0]
+    color0 = block_colors[0]
 
     max_zeroes_left_side = min(
         max_zeroes_left_side_from_input, max_zeroes_left_side_from_slice
@@ -144,14 +144,14 @@ def generate_count_from_slice(
         i0 = 0
     for i in range(i0, max_zeroes_left_side + 1):
         # We checked that the white blocks are allowed, now check, if the colored block is allowed:
-        if all(slice[i : (i + l), c]):
+        if all(slice[i : (i + length0), color0]):
             tmp = generate_count_from_slice(
-                n=n - i - l,
+                n=n - i - length0,
                 n_colors=n_colors,
                 block_lengths=block_lengths[1:],
                 block_colors=block_colors[1:],
-                slice=totuple(slice[i + l :,]),
-                previous_color=c,
+                slice=totuple(slice[i + length0 :,]),
+                previous_color=color0,
             )
             count += tmp
     return count
@@ -176,14 +176,14 @@ def generate_color_possible_from_slice(
     max_zeroes_left_side_from_input = n - sum(block_lengths) - n_same_colored_neighbours
 
     # Find the first index where no white is allowed
-    tmp = np.nonzero(slice[:, WHITE] == False)[0]
+    tmp = np.nonzero(not slice[:, WHITE])[0]
     if len(tmp) > 0:
         max_zeroes_left_side_from_slice = tmp[0]
     else:
         max_zeroes_left_side_from_slice = n
 
-    l = block_lengths[0]
-    c = block_colors[0]
+    length0 = block_lengths[0]
+    color0 = block_colors[0]
 
     max_zeroes_left_side = min(
         max_zeroes_left_side_from_input, max_zeroes_left_side_from_slice
@@ -194,20 +194,20 @@ def generate_color_possible_from_slice(
         i0 = 0
     for i in range(i0, max_zeroes_left_side + 1):
         # We checked that the white blocks are allowed, now check, if the colored block is allowed:
-        if all(slice[i : (i + l), c]):
+        if all(slice[i : (i + length0), color0]):
             a2 = generate_color_possible_from_slice(
-                n=n - i - l,
+                n=n - i - length0,
                 n_colors=n_colors,
                 block_lengths=block_lengths[1:],
                 block_colors=block_colors[1:],
-                slice=totuple(slice[i + l :, :]),
-                previous_color=c,
+                slice=totuple(slice[i + length0 :, :]),
+                previous_color=color0,
             )
 
             # Build result for this starting position
-            a1 = np.zeros((i + l, n_colors), dtype=np.bool_)
+            a1 = np.zeros((i + length0, n_colors), dtype=np.bool_)
             a1[:i, WHITE] = True  # Mark whites as possible
-            a1[i : (i + l), c] = True  # Mark current color as possible
+            a1[i : (i + length0), color0] = True  # Mark current color as possible
             a = np.concatenate((a1, a2), axis=0)
 
             # OR with previous results
